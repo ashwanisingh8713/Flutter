22feeb4a14a9c0c0a8d90656df328d7e4a3d5dba
ashwani <ashwani.singh@ninestars.in>
2022-05-21 20:46:41 +0530

    first commit

---
+++ lib/ui/splash.dart
@@ -0,0 +1,50 @@
+
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../app_state.dart';
+
+const String LoggedInKey = 'LoggedIn';
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  bool _initialized = false;
+  AppState appState;
+
+  @override
+  Widget build(BuildContext context) {
+    appState = Provider.of<AppState>(context);
+    final query = MediaQuery.of(context);
+    final size = query.size;
+    final itemWidth = size.width;
+    final itemHeight = itemWidth * (size.width / size.height);
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Container(
+            child: Image.asset('assets/images/splash.png',
+                width: itemWidth, height: itemHeight),
+          ),
+        ),
+      ),
+    );
+  }
+
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (!_initialized) {
+      _initialized = true;
+      Timer(const Duration(milliseconds: 2000), () {
+        appState.setSplashFinished();
+      });
+    }
+  }
+}
